af15c78e812915b4a1bfa8c49921af48b5274f7f
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2025-02-13 03:53:32 +0530

    added constrains

---
+++ 03-Create-Database-Tables/10-Constrains.sql
@@ -0,0 +1,32 @@
+UNIQUE
+CREATE TABLE contacts (
+	name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE
+);
+ 
+INSERT INTO contacts (name, phone)
+VALUES ('billybob', '8781213455');
+
+
+CHECK
+CREATE TABLE users (
+	username VARCHAR(20) NOT NULL,
+    age INT CHECK (age > 0)
+);
+ 
+CREATE TABLE palindromes (
+  word VARCHAR(100) CHECK(REVERSE(word) = word)
+)
+
+CONSTRAINT
+CREATE TABLE users2 (
+    username VARCHAR(20) NOT NULL,
+    age INT,
+    CONSTRAINT age_not_negative CHECK (age >= 0)
+);
+ 
+CREATE TABLE palindromes2 (
+  word VARCHAR(100),
+  CONSTRAINT word_is_palindrome CHECK(REVERSE(word) = word) -- passing custom Name
+);
+
